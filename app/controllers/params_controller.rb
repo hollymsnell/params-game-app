a6188b037744eafa72d7606d590c6561e58fbddc
hollymsnell <hollymsnell@gmail.com>
2022-03-09 18:00:06 -0700

    edited controller and routes for accuracy

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -2,5 +2,7 @@ class ParamsController < ApplicationController
 
   def query_params_method
     input_name = params["name"]
-    render json: {message: "#{input_name} is the coolest!", message2: "Your number is #{'x'}"}
+    render json: {message: "#{input_name} is the coolest!", message2: "Your number is #{params['x']}"}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-get "/params_game" => "params_controller#query_params_method"
+get "/params_game" => "params#query_params_method"
 end
