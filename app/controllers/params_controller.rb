c5aa6bbeb2520c2c26da2593c0f9c7718fdf7292
hollymsnell <hollymsnell@gmail.com>
2022-03-09 17:44:41 -0700

    added query params and routes

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,2 +1,6 @@
 class ParamsController < ApplicationController
+
+  def query_params_method
+    input_name = params["name"]
+    render json: {message: "#{input_name} is the coolest!", message2: "Your number is #{'x'}"}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+get "/params_game" => "params_controller#query_params_method"
 end
